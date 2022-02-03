ace741eb279031b5082f977f086470bfceafc6d4
Cameron <cameronhunt88@hotmail.com>
2022-02-03 17:07:46 +0000

    started work on navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./CSS/App.css";
+import React from "react";
+import Navbar from "./components/Navbar.js";
+import "./CSS/style.css";
 
 function App() {
-  return "hello";
+  return (
+    <main>
+      <Navbar />
+    </main>
+  );
 }
 
 export default App;
