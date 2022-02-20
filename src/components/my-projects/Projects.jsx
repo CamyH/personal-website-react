62a5c9c66a927c6f559da9dacd7b5267855e66df
Cameron <cameronhunt88@hotmail.com>
2022-02-20 16:10:15 +0000

    added first project image

--- src/components/my-projects/Projects.jsx
+++ src/components/my-projects/Projects.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./projects.css";
+import f2 from "./f2-program.png";
 
 function Projects() {
     return (
@@ -7,6 +8,9 @@ function Projects() {
             <section id='projects-page'>
                 <div id='header'>
                     <h1 id='projects-header'>Projects</h1>
+                </div>
+                <div id='projects'>
+                    <img src={f2} id='project-1' class="img-fluid" />
                 </div>
             </section>
         </React.Fragment>
