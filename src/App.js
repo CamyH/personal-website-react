cb9b7350da83f8721752c8aa8eeb7c9079c234a3
Cameron <cameronhunt88@hotmail.com>
2022-02-26 09:37:29 +0000

    updated imports for router package

--- src/App.js
+++ src/App.js
@@ -4,23 +4,17 @@ import "./components/landing-page/landing-page.css";
 import LandingPage from "./components/landing-page/Landing-Page";
 import Projects from "./components/my-projects/Projects";
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Link,
-  Redirect,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<LandingPage />} />
+				<Route path="/projects" element={<Projects />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
