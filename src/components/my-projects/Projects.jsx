fb1f9c8dc0b6b34de44c69f6588ec4d9ba121871
Cameron <cameronhunt88@hotmail.com>
2022-02-26 09:29:24 +0000

    updated _blank to new + added alt tags

--- src/components/my-projects/Projects.jsx
+++ src/components/my-projects/Projects.jsx
@@ -3,6 +3,7 @@ import "./projects.css";
 import f2 from "./f2-program.png";
 import weather from "./weather-app.png";
 import cyoag from "./vikingr-adventure.png";
+import github from "./github-logo-white.png";
 
 function Projects() {
     return (
@@ -12,10 +13,10 @@ function Projects() {
                     <h1 id='projects-header'>Projects</h1>
                 </div>
                 <div id='projects'>
-                        <a href='https://github.com/CamyH/F2-League-Leaderboard' target="_blank"><img src={f2} id='project-1' class="projects img-fluid" /></a>
-                        <a href='https://github.com/CamyH/WeatherApp' target="_blank"><img src={weather} id="project-2" class="projects img-fluid" /></a>
-                        <a href='https://github.com/CamyH/A-Vikingr-Adventure-CYOAG' target="_blank"><img src={cyoag} id="project-3" class="projects img-fluid" /></a>
-                    </div>
+                    <a href='https://github.com/CamyH/F2-League-Leaderboard' target="new"><img src={f2} id='project-1' class="projects img-fluid" alt='f2 program' /></a>
+                    <a href='https://github.com/CamyH/WeatherApp' target="new"><img src={weather} id="project-2" class="projects img-fluid" alt='weather app' /></a>
+                    <a href='https://github.com/CamyH/A-Vikingr-Adventure-CYOAG' target="new"><img src={cyoag} id="project-3" class="projects img-fluid" alt='adventure game' /></a>
+                </div>
             </section>
         </React.Fragment>
     )
