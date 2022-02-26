badd9850246ee01b0f391fae3f0102b0a4b71b50
Cameron <cameronhunt88@hotmail.com>
2022-02-26 08:57:40 +0000

    first row of project images added

--- src/components/my-projects/Projects.jsx
+++ src/components/my-projects/Projects.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import "./projects.css";
 import f2 from "./f2-program.png";
+import weather from "./weather-app.png";
+import cyoag from "./vikingr-adventure.png";
 
 function Projects() {
     return (
@@ -10,7 +12,9 @@ function Projects() {
                     <h1 id='projects-header'>Projects</h1>
                 </div>
                 <div id='projects'>
-                    <img src={f2} id='project-1' class="img-fluid" />
+                    <img src={f2} id='project-1' class="projects img-fluid" />
+                    <img src={weather} id="project-2" class="projects img-fluid" />
+                    <img src={cyoag} id="project-3" class="projects img-fluid" />
                 </div>
             </section>
         </React.Fragment>
