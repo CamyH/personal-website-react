7e3cba53ca9320161facc270dc7ab71f34d3b39a
Cameron <cameronhunt88@hotmail.com>
2022-03-06 10:42:06 +0000

    added most of about page with placeholders

--- src/components/about-me/About-Me.jsx
+++ src/components/about-me/About-Me.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import "./about-me.css";
 
 function AboutMe() {
-    return (
-        <React.Fragment>
-
-        </React.Fragment>
-    );
+	return (
+		<React.Fragment>
+			<h1 id="about-header">About Me</h1>
+			<div id="content-wrapper">
+				<img id="profile-pic" src="placeholder.png" alt="placeholder"></img>
+				<p class="about-text">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ut
+					libero non leo condimentum gravida. Pellentesque imperdiet erat et
+					ipsum varius facilisis. Suspendisse eu imperdiet mi. Nulla sed nibh
+					nec felis dapibus semper. Nullam sodales, quam sit amet luctus
+					elementum, lorem odio dignissim leo, rutrum aliquam augue ipsum ac
+					nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+					posuere cubilia curae; Aliquam bibendum pretium consequat. Integer
+					quis orci fermentum, placerat metus a, rutrum magna.
+				</p>
+			</div>
+		</React.Fragment>
+	);
 }
 
 export default AboutMe;
